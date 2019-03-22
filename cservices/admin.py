bb90f2959613a739efddca8c2b2210bb95e95cb0
Nadica08 <nadiabc3@gmail.com>
2019-03-22 16:31:31 -0700

    Changes to Index verbiage by  Nadia & Darla. Next, I made changes to views and models that completed the class and request for a functioning admin acct & application saving process.

--- cservices/admin.py
+++ cservices/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.db import models
+from django import forms
 # Register your models here.
+from .models import Application
+
+admin.site.register(Application)
