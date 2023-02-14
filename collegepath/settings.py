327db5a25b5ee02f102aa0b771d52228db93f04a
Darla Brown <darlanebrown@gmail.com>
2023-02-14 12:05:52 -0800

    railway deployment ready

--- collegepath/settings.py
+++ collegepath/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3#ty2jqp3cc#2n9x7nl5azmg+ds6jp8!mfn!7hm50p=j*!k6=*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
